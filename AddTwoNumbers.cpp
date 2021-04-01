8f13b2fdbc6b245b5b321701f40541ab37954af1
seymurkafkas <seymurkafkas@gmail.com>
2021-04-01 21:14:38 +0100

    Skeleton

--- AddTwoNumbers.cpp
+++ AddTwoNumbers.cpp
@@ -13,5 +13,28 @@ class Solution
 public:
   ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
   {
+
+    int carry = 0;
+    while (true)
+    {
+      if (!l2 && !l1)
+      {
+        break;
+      }
+
+      int res = 0;
+
+      if (l1)
+      {
+
+        l1 = l1->next;
+      }
+
+      if (l2)
+      {
+
+        l2 = l2->next;
+      }
+    }
   }
 };
