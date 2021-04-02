1ece23de587ab4770946fe807ac272c81622f85f
seymurkafkas <seymurkafkas@gmail.com>
2021-04-02 12:31:17 +0100

    Q3 - First Draft

--- Q3.cpp
+++ Q3.cpp
@@ -1,23 +1,40 @@
 #include <string>
 #include <iostream>
+#include <unordered_map>
 
 class Solution
 {
 public:
     int lengthOfLongestSubstring(std::string s)
     {
-
-        for (int i = 0; i < s.length(); ++i)
+        std::unordered_map<char, unsigned int> hashMap;
+        int currentResult = 0;
+        int maxResult = 0;
+        for (int i = 0, j = 0; j < s.length(); ++j)
         {
-            std::cout << s[i];
+            auto it = hashMap.find(s[j]);
+            if (it == hashMap.end() || it->second < i)
+            {
+                std::cout << "X";
+                hashMap[s[j]] = j;
+                currentResult++;
+            }
+            else
+            {
+                std::cout << "I";
+                i = hashMap[s[j]] + 1;
+                maxResult = std::max(maxResult, currentResult);
+                currentResult = j - i + 1;
+            }
         }
-        return 0;
+
+        return std::max(maxResult, currentResult);
     }
 };
 
 int main()
 {
     Solution a;
-    a.lengthOfLongestSubstring("hellothere");
+    std::cout << a.lengthOfLongestSubstring("aaaaaaaba");
     return 0;
 }
