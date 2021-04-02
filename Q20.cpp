2e603e754ce540cd56c2e040a5a98f774c513f57
seymurkafkas <seymurkafkas@gmail.com>
2021-04-02 22:06:47 +0100

    Q20 - First Draft

--- Q20.cpp
+++ Q20.cpp
@@ -9,9 +9,18 @@ public:
         std::stack<char> pushdownAutomaton;
         for (const auto &currentChar : s)
         {
-            if(s== '(' || s == '{' || s== '['){
-                pushdownAutomaton.push
+            if (s == '(' || s == '{' || s == '[')
+            {
+                pushdownAutomaton.push(s);
+            }
+            else
+            {
+                if (pushdownAutomaton.empty() || pushdownAutomaton.pop() != oppositeBracket(currentChar))
+                {
+                    return false;
+                }
             }
         }
+        return pushdownAutomaton.empty();
     }
 };
