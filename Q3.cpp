5a2c22c1f091ebff82ffa5a984f1f4c108e80acc
seymurkafkas <seymurkafkas@gmail.com>
2021-04-02 10:11:58 +0100

    Q2- Main

--- Q3.cpp
+++ Q3.cpp
@@ -1,15 +1,23 @@
+#include <string>
+#include <iostream>
+
 class Solution
 {
 public:
     int lengthOfLongestSubstring(std::string s)
     {
 
-
-
-
-
-
-
-        
+        for (int i = 0; i < s.length(); ++i)
+        {
+            std::cout << s[i];
+        }
+        return 0;
     }
 };
+
+int main()
+{
+    Solution a;
+    a.lengthOfLongestSubstring("hellothere");
+    return 0;
+}
