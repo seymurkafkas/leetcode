61040e6badb7feba7803507a123324ddcc1c958e
seymurkafkas <seymurkafkas@gmail.com>
2021-04-03 21:10:19 +0100

    Q89 - First Draft

--- Q89.cpp
+++ Q89.cpp
@@ -1,7 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+
 class Solution
 {
 public:
-    vector<int> grayCode(int n)
+    std::vector<int> grayCode(int n)
     {
+
+        std::vector<int> grayCodeVector;
+        grayCodeVector.push_back(0);
+        for (int i = 1; i < 1 << n; ++i)
+        {
+
+            grayCodeVector.push_back(grayCodeVector[i - 1] ^ getXOROperand(i));
+        }
+
+        return grayCodeVector;
+    }
+
+    int getXOROperand(int currentVectorIndex)
+    {
+
+        int currentDivisor = 2;
+        for (int potentialBitIndex = 1; potentialBitIndex <= 16; potentialBitIndex++)
+        {
+
+            if (currentVectorIndex % currentDivisor == (currentDivisor) / 2)
+            {
+                return currentDivisor / 2;
+            }
+            else
+            {
+                currentDivisor *= 2;
+            }
+        }
     }
 };
+
+int main()
+{
+
+    Solution solver;
+    std::cout << solver.getXOROperand(8);
+
+    return 0;
+}
