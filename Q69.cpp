73d687e09d1cdaffb4af3fe0f67a1aaf3c7bd431
seymurkafkas <seymurkafkas@gmail.com>
2021-04-03 14:14:41 +0100

    Q69 - Accepted

--- Q69.cpp
+++ Q69.cpp
@@ -6,21 +6,49 @@ public:
     int mySqrt(int x)
     {
 
-        float guess = x / 2.0;
-        while (guess * guess - x >= 1 || guess * guess < x)
+        if (x == 0 || x == 1)
+        {
+            return x;
+        }
+
+        long long int currentLowerBound = 1;
+        long long int currentUpperBound = x;
+        long long int currentAnswer, median;
+
+        while (currentLowerBound <= currentUpperBound)
         {
 
-            guess = (guess + x / guess) / 2;
+            median = currentLowerBound + (currentUpperBound - currentLowerBound) / 2;
+
+            if (median * median == x)
+            {
+                currentAnswer = median;
+                break;
+            }
+
+            else if (median * median - x < 0)
+            {
+                currentLowerBound = median + 1;
+                currentAnswer = median;
+            }
+
+            else
+            {
+                currentUpperBound = median - 1;
+            }
         }
-        return (int)guess;
+
+        return currentAnswer;
     }
-};
+}
+
+;
 
 int main()
 {
 
     Solution solver;
-    std::cout << solver.mySqrt(1);
+    std::cout << solver.mySqrt(2147395599);
 
     return 0;
 }
