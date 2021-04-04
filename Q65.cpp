6a50f8fb3fcc5f3d737dcd5f50a3a5e39da034ce
seymurkafkas <seymurkafkas@gmail.com>
2021-04-04 13:01:19 +0100

    Q65 - First Draft

--- Q65.cpp
+++ Q65.cpp
@@ -1,7 +1,14 @@
+#include <string>
+#include <iostream>
+#include <regex>
+
 class Solution
 {
 public:
-    bool isNumber(string s)
+    bool isNumber(std::string s)
     {
+        std::regex regex("[+-]?[0-9]*[.]?[0-9]*([eE][0-9]+)?");
+
+        return std::regex_match(s, regex);
     }
 };
