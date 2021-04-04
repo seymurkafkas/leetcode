bb9f714373999efd42e5433aff9e70c3834b4863
seymurkafkas <seymurkafkas@gmail.com>
2021-04-04 13:22:26 +0100

    Q65 - Second Draft

--- Q65.cpp
+++ Q65.cpp
@@ -7,8 +7,21 @@ class Solution
 public:
     bool isNumber(std::string s)
     {
-        std::regex regex("[+-]?[0-9]*[.]?[0-9]*([eE][0-9]+)?");
+        if (s == ".")
+        {
+            return false;
+        }
+        std::regex regex("[+-]?(([0-9]+[.]?[0-9]*)|([0-9]*[.]?[0-9]+))([eE][+-]?[0-9]+)?");
 
         return std::regex_match(s, regex);
     }
 };
+
+int main()
+{
+
+    Solution solver;
+    std::cout << solver.isNumber("-324.e123");
+
+    return 0;
+}
