6d880b6bc073ef366bc4b587e1e4913e63e52b9c
seymurkafkas <seymurkafkas@gmail.com>
2021-04-06 19:07:45 +0100

    Q218- Initialised

---
+++ SkylineProblem.cpp
@@ -0,0 +1,13 @@
+class Solution
+{
+public:
+    vector<vector<int>> getSkyline(vector<vector<int>> &buildings)
+    {
+    }
+};
+
+int main()
+{
+    vector<vector<int>> buildings;
+    return 0;
+}
