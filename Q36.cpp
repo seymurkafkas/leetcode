d83bf7437c4dc9c2caf0e10b6537d21f80716c50
seymurkafkas <seymurkafkas@gmail.com>
2022-03-10 20:33:51 +0300

    Q36 final draft

--- Q36.cpp
+++ Q36.cpp
@@ -1,6 +1,5 @@
 #include <vector>
 #include <iostream>
-#include <unordered_set>
 
 using namespace std;
 
@@ -10,34 +9,36 @@ class Solution
 public:
     bool isValidSudoku(vector<vector<char>> &board)
     {
-        std::unordered_set<char> grid_first, grid_second, grid_third;
-        std::vector<std::unordered_set<char>> columnContents;
+        std::vector<bool> grid_first(9, false), grid_second(9, false), grid_third(9, false);
+        std::vector<std::vector<bool>> columnContents(9, std::vector<bool>(9, false));
 
         for (int row = 0; row < 9; ++row)
         {
-            std::unordered_set<char> currentRowContents;
+            std::vector<bool> currentRowContents(9, false);
 
             if (row % 3 == 0)
             {
-                grid_first.clear();
-                grid_second.clear();
-                grid_third.clear();
+                std::fill(grid_first.begin(), grid_first.end(), false);
+                std::fill(grid_second.begin(), grid_second.end(), false);
+                std::fill(grid_third.begin(), grid_third.end(), false);
             }
 
             for (int column = 0; column < 9; ++column)
             {
                 char currentCharacter = board[row][column];
-                if (currentCharacter == ',')
+
+                if (currentCharacter == '.')
                     continue;
 
-                std::unordered_set<char>::const_iterator iteratorToFoundElement = currentRowContents.find(currentCharacter);
-                if (iteratorToFoundElement == currentRowContents.end())
+                int currentCharacterIndex = (int)currentCharacter - '1';
+
+                if (currentRowContents[currentCharacterIndex])
                     return false;
 
-                currentRowContents.insert(currentCharacter);
+                currentRowContents[currentCharacterIndex] = true;
 
-                int currentGroupNumber = (int)(column / 3) * 3;
-                const std::unordered_set<char> *currentGroupContentsPtr;
+                int currentGroupNumber = (int)(column / 3);
+                std::vector<bool> *currentGroupContentsPtr;
 
                 switch (currentGroupNumber)
                 {
@@ -52,13 +53,34 @@ public:
                     break;
                 }
 
-                iteratorToFoundElement = currentGroupContentsPtr->find(currentCharacter);
-
-                if (iteratorToFoundElement == currentGroupContentsPtr->end())
+                if ((*currentGroupContentsPtr)[currentCharacterIndex])
                     return false;
+
+                (*currentGroupContentsPtr)[currentCharacterIndex] = true;
+
+                if (columnContents[column][currentCharacterIndex])
+                    return false;
+
+                columnContents[column][currentCharacterIndex] = true;
             }
         }
 
         return true;
     }
 };
+
+int main()
+{
+
+    vector<vector<char>> board{{'8', '3', '.', '.', '7', '.', '.', '.', '.'},
+                               {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                               {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                               {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                               {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                               {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                               {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                               {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                               {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
+    Solution solver;
+    bool isBoardValid = solver.isValidSudoku(board);
+}
