2d80dc082ee0ef0edbd9bd89d237405fad86140d
seymurkafkas <seymurkafkas@gmail.com>
2022-05-22 13:40:04 +0300

    Q292 accepted

--- Q292.ts
+++ Q292.ts
@@ -1,21 +1,3 @@
 function canWinNim(n: number): boolean {
-  if (n <= 3) return true;
-
-  const bufferLength = 3;
-  const buffer = new Array<boolean>(bufferLength);
-
-  buffer.fill(true);
-
-  for (let i = 4; i <= n; i++) {
-    const canWin = !buffer[0] || !buffer[1] || !buffer[2];
-
-    for (let j = 1; j < 3; j++) buffer[j - 1] = buffer[j];
-    buffer[2] = canWin;
-  }
-
-  return buffer[2];
+  return n % 4 !== 0;
 }
-
-(function main() {
-  console.log(canWinNim(4));
-})();
